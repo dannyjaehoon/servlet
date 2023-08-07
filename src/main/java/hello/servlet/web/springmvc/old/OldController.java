6656e0331bb1ee69b677f3f3953cd5c47714212b
jaehoonlee <jaelee9212>
2023-08-07 20:34:42 +0900

    Completed the Session 5 - learned the structure of Spring MVC

---
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,22 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    // 핸들러 매핑에서 이 컨트롤러를 찾을 수 있어야된다.
+    // 스프링빈의 이름으로 핸들러를 찾을 수 있는 핸들러 매핑이 필요하다.
+    // 핸들러매핑을 통해서 찾은 핸들러를 실행할 수 있는 핸들러 어댑터가 필요하다
+    // Controller 인터페이스를 실행할수 있는 핸들러 어댑터를 찾고 실행해야된다.
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return new ModelAndView("new-form");
+    }
+}
